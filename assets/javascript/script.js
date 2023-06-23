99488f00f7500c547d5f8e9f3ebc08e088e9b5a6
LiamEdwards931 <>
2023-06-23 09:05:20 +0000

    add script to easily change animation state with object keywords

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,23 +9,84 @@ playerImage.src = 'assets/spritesheets/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 
-//variables to change the location of the crop on the spritesheet
-let frameX = 0;
-let frameY = 0;
-//
 let gameFrame = 0;
 // this value changes to speed up or slow down animation (higher number = slower animation)
 let staggerFrame = 5;
+// array to store the animation frames
+const spriteAnimation = [];
+//array of objects containing the animation types
+const animationState = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'sprint',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 10,
+    },
+    {
+        name: 'lie down',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'crouch',
+        frames: 7,
+    },
+    {
+        name: 'jump into lie down',
+        frames: 10,
+    },
+    {
+        name: 'look up',
+        frames: 4,
+    }
+];
+
+/**
+ * Names each object 'state' and gets it's index, each one is bumped by one with the 'a++'
+ * pushes the calculated positions back into the frames.loc array and adds to the objects
+ * gives the spriteAnimation variable the value of the objects in the frames Array.
+ */
+animationState.forEach((state, index) => {
+    let frames = {
+        loc:[],
+    }
+    for(let a = 0; a < state.frames; a++){
+        let positionX = a * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimation[state.name] = frames;
+});
+console.log(spriteAnimation);
 /**
  * Animates the main "dog character"
  */
 function animate(){
     ctx.clearRect(0, 0, canvas_Width, canvas_Height);
     // code below uses integers to calculate the frame - value 6 changes based on how many sprites are in the row.
-    let position = Math.floor(gameFrame/staggerFrame) % 6;
-    frameX = spriteWidth * position;
+    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimation['idle'].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimation["idle"].loc[position].y;
+    
     //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh) - (image src variable, sx&y position of crop, sw & sh width and height of crop tool, d's are the same but for the cropped image)
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight , spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(playerImage, frameX, frameY , spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     gameFrame++
     requestAnimationFrame(animate);
 };
