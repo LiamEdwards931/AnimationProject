472d2c4d6a7af04ff6c1e1273130bb1351aa1f5a
LiamEdwards931 <>
2023-06-24 17:10:04 +0000

    add core html,css the parallax background page

---
+++ assets/javascript/Background.js
@@ -0,0 +1,7 @@
+// constant canvas structure
+const canvas = document.getElementById('canvas2');
+const ctx = canvas.getContext('2D');
+const canvasWidth = canvas.width = 800;
+const canvasHeight = canvas.height = 600;
+//game speed variable
+let gameSpeed = 5;
