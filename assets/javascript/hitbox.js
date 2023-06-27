a2b80757e0b943480ec9ba65e1ad7e0415773f85
LiamEdwards931 <>
2023-06-27 10:47:14 +0000

    add click effect and sound to the animation

--- assets/javascript/hitbox.js
+++ assets/javascript/hitbox.js
@@ -10,18 +10,22 @@ const explosions = [];
 //class constructor for Explosion animation
 class Explosion {
     constructor(x, y){
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.7; //keep the values the same for aspect ratio - increases/decreases size of animation
         this.height = this.spriteHeight * 0.7 ;
+        this.x = x; //offsets x for the click animation
+        this.y = y; // offsets y for the click animation
         this.image = new Image(); // creates new html img elements
         this.image.src = "assets/spritesheets/boom.png";
         this.frame = 0; // amount the spritesheet jumps to the right by for animation
         this.timer = 0; // variable for the animation speed on click.
+        this.angle = Math.random() * 6.2 //angle for the explosion
+        this.sound = new Audio();
+        this.sound.src = 'assets/sfx/Ice attack 2.wav'
     }
     update(){
+        if(this.frame === 0) this.sound.play();
         this.timer++ // increments timer speed
         if(this.timer % 15 === 0){// code for run this every 10 frames - higher number slows the animation speed.
             this.frame++ // increments the spritesheet 
@@ -29,21 +33,36 @@ class Explosion {
     }
        
     draw(){//multiply this.spriteWidth by this frame to trigger the animation loop
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 -this.width/2, 0 - this.height/2, this.width, this.height);
+        ctx.restore();
     }
 }
 // event listener to call the class 'explosion' - offset the x and y to account for margin/padding on the page with the variable above
+
+// can change the 'click' for different effects
 window.addEventListener('click', function(b){
+    createAnimation(b);
+});
+
+//function for the click event.
+function createAnimation(b){
     let positionX = b.x - canvasPosition.left;
     let positionY = b.y - canvasPosition.top;
     explosions.push(new Explosion(positionX, positionY));
-});
+}
 //function for clearing the canvas and calling the explosion array.
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for(let i = 0; i < explosions.length; i++){
         explosions[i].update();
         explosions[i].draw();
+        if(explosions[i].frame > 5){
+            explosions.splice(i, 1);
+            i--
+        }
     }
     console.log(explosions);
     requestAnimationFrame(animate);
