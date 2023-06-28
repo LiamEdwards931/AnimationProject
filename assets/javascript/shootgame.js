61d09b58da810711d80edbcfb2ae7aacdaecd66b
LiamEdwards931 <>
2023-06-28 00:46:49 +0000

    add code to adjust the birds flight patterns and bounce if they touch top or bottom of canvas

--- assets/javascript/shootgame.js
+++ assets/javascript/shootgame.js
@@ -13,9 +13,9 @@ class Raven {
     constructor() {
         this.spriteWidth = 271;
         this.spriteHeight = 194;
-        this.width = this.spriteWidth * this.sizeModifier; // spritesheet width;
-        this.height = this.spriteHeight * this.sizeModifier; // spritesheet Height.
         this.sizeModifier = Math.random() * 0.6 + 0.4; // variable to change the size of the sprites
+        this.width = this.spriteWidth * this.sizeModifier; // spritesheet width; - ensure the variable you are '*' is above the code here e.g sizeMod needs to above this to work.
+        this.height = this.spriteHeight * this.sizeModifier; // spritesheet Height.
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height); // creates raven at random y co-ordinates on the page
         this.directionX = Math.random() * 5 + 3; // Move speed of the ravens
@@ -23,15 +23,34 @@ class Raven {
         this.image = new Image();
         this.image.src = "assets/enemyimages/raven.png";
         this.markForDelete = false;
-        this.frame = 0;
+        this.frame = 0; // first animation frame for the raven.
+        this.maxFrame = 4; //last animation frame for the raven.
+        this.timeSinceFlap = 0;
+        this.flapInterval = Math.random() * 50 + 50; // randomises the flapping speeds of the birds. 
     }
-    update() {
+    update(deltatime) {
+        if (this.y < 0 || this.y > canvas.height - this.height) { // if statement to make birds bounce off the top and bottom of screen
+            this.directionY = this.directionY * -1;
+        }
+
         this.x -= this.directionX;
+        this.y += this.directionY;
         if (this.x < 0 - this.width) this.markForDelete = true; // if position is less than 0 set value to true so can be removed in the filter in animate.
+
+        this.timeSinceFlap += deltatime; //unifies flapping speed across all speed devices.
+
+        this.timeSinceFlap += deltatime; //unifies flapping speed across all speed devices. delta time is the time between last frame and current frame.
+        if (this.timeSinceFlap > this.flapInterval) {
+            if (this.frame > this.maxFrame) this.frame = 0; // resets the loops on the spritesheet to animate.
+            else this.frame++;
+            this.timeSinceFlap = 0; //resets the flap time back to 0;
+        }
+
+
     }
     draw() {
         ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 /**
@@ -47,7 +66,7 @@ function animate(timestamp) {
         ravens.push(new Raven());
         timeToNextRaven = 0;
     }
-    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markForDelete);
     requestAnimationFrame(animate);
